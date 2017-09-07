9f160fd034c58aaab1488792a2cc94e08c1091cf
Anthony Lombardo <c_anlomb@qualcomm.com>
2017-09-06 17:59:31 -0700

    Modified GRID startup scripts to use matching selenium grid server

--- src/main/resources/grid/start-node.cmd
+++ src/main/resources/grid/start-node.cmd
@@ -1,3 +1,3 @@
 echo Starting Selenium Node...
 
-java -jar -Dwebdriver.chrome.driver="../drivers/chrome/chrome_win.exe" -Dwebdriver.ie.driver="../drivers/ie/ie_win64.exe" -Dwebdriver.gecko.driver="../drivers/firefox/geckodriver.exe" selenium-server-standalone-3.0.1.jar -role node -nodeConfig "nodeConfig.json"
+java -jar -Dwebdriver.chrome.driver="../drivers/chrome/chrome_win.exe" -Dwebdriver.ie.driver="../drivers/ie/ie_win64.exe" -Dwebdriver.gecko.driver="../drivers/firefox/geckodriver.exe" selenium-server-standalone-3.5.3.jar -role node -nodeConfig "nodeConfig.json"
--- src/main/resources/grid/start-node.sh
+++ src/main/resources/grid/start-node.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo Starting Selenium Node on Linux ENV...
 
-java -jar -Dwebdriver.chrome.driver="../drivers/chrome/chrome_linux_64" -Dwebdriver.gecko.driver="../drivers/firefox/geckodriver" selenium-server-standalone-3.0.1.jar -role node -nodeConfig "defaultNodeConfig.json"
+java -jar -Dwebdriver.chrome.driver="../drivers/chrome/chrome_linux_64" -Dwebdriver.gecko.driver="../drivers/firefox/geckodriver" selenium-server-standalone-3.5.3.jar -role node -nodeConfig "defaultNodeConfig.json"
