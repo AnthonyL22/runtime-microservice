e43273c26a7230b127a99781a3081d7c26c7b520
c-anlomb <c_anlomb@qualcomm.com>
2017-09-14 10:35:10 -0700

    Added PhantomJS to start node scripts.

--- src/main/resources/grid/start-node.sh
+++ src/main/resources/grid/start-node.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo Starting Selenium Node on Linux ENV...
 
-java -jar -Dwebdriver.chrome.driver="../drivers/chrome/chrome_linux_64" -Dwebdriver.gecko.driver="../drivers/firefox/geckodriver" selenium-server-standalone.jar -role node -nodeConfig "defaultNodeConfig.json"
+java -jar -Dwebdriver.chrome.driver="../drivers/chrome/chrome_linux_64" -Dwebdriver.gecko.driver="../drivers/firefox/geckodriver" -Dphantomjs.binary.path="../drivers/phantomjs/phantomjs" selenium-server-standalone.jar -role node -nodeConfig "defaultNodeConfig.json"
